228a450dfe6c4cb6355ff96d618475755e9fec7e
quietgrove <quietgroveatelier@gmail.com>
2026-05-17 20:37:31 +0900

    v1.1.0 Relese後の修正

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ OyaShiftKey は、quietgrove が育てている
 ---
 
 📦 最新版のダウンロードは Releases からどうぞ  
-https://github.com/quietgrove/OyaShiftKey/releases
+https://github.com/groveatelier/OyaShift/releases/tag/六式-v1.1.0
 
 ---
 
--- Roku-shiki/chromos/common/RokushikiIME.js
+++ Roku-shiki/chromos/common/RokushikiIME.js
@@ -1,4 +1,4 @@
-/*  2026.05.15 12:00
+/*  2026.05.17 20:00
   Oya Key shift keyboard ver 5.0 (自作キーボード用)
     
     >> Spcial keys << insidebuf.length > 0 
@@ -145,77 +145,6 @@ function changeAndClear(){
     if( keycondition < 1024 && cursolbuf.length > 0 ) fixAll();    //  日モードなら全確定.
     changeKanaMode();   // かなモード切り替え
     InitialKana();
-}
-
-// SS keyDown event
-function SSKeyDown(engineID, keyData){
-    let enact = false;
-//    console.log(`sKD:(${keyData.key}|${keyData.code})`);
-    if( thumbShift( keyData ) ){            // 親指シフト判断処理.
-        if( !convKana[0] ) keyValidiate();  // 有効キー&入力確定.
-        enact = true;
-    } 
-    else if( insidebuf.length > 0 ) {    // insidebuf(or cCandidate) が存在する時の処理
-//        console.log(`ssKD+:(${keyData.key}|${keyData.code})`);
-        // Shift Space と タブ は先頭確定.
-        if( keyData.key === " " || keyData.key === "Tab" ){
-            if( keyData.shiftKey ) fixOne();        // Shift付きは先頭確定.
-            else setOtherCandidate( 1 );            // 先頭変換.
-            enact = true;
-        } else {
-            enact = true;
-            switch( keyData.key ){
-                case "Enter":                   // Enter は 全確定 か キー処理、shift があれば先頭確定. 
-                    if( keyData.shiftKey || compoinfo < 0 ) fixOne();  // 先頭確定.
-                    else fixAll();                                      // 全確定.
-                    break;
-                case "Up":                          // 上下矢印 は先頭変換.
-                case "Down":
-                    let addinx = ( keyData.key === "Up") ? -1 : 1;
-                    setOtherCandidate( addinx );    // 先頭変換
-                    break;
-                case "Right":                       // 右矢印 は カーソル移動 キー処理. 
-                    compoinfo++;                    // カーソル右へ.
-                    if( compoinfo > 0 ) compoinfo = 0;
-                    showComposition();
-                    break;
-                case "Left":                        // 左矢印 は カーソル移動 キー処理. 
-                    compoinfo--;                    // カーソル左へ.
-                    if( insidebuf.length + compoinfo < 0 ) compoinfo = -insidebuf.length;
-                    showComposition();
-                    break;
-                case "Backspace":                   // Backspeceの入力 (変換候補有りの時のみ処理する) 
-                    BackOne();
-                    henkanAri = false;
-                    if( insidebuf.length > 0 ) rokushikiIME();
-                    else clearCompoAndCand();
-                    break;
-                case "BrightnessUp":                // Brightness upの入力 (カタカナ変換) 
-                    translateKanaKana( true )
-                    showComposition();
-                    break;
-                case "BrightnessDown":              // Brightness Downの入力 (ひらがな変換)
-                    translateKanaKana( false );
-                    showComposition();
-                    break;
-                case "Esc":                         // 未変換化
-                    UndoConvert( false );           // ESCキーモードで実行.
-                    break;
-                case "\"":                      // 一文字確定. Double Quate
-                    OneLeCommit();              // 一文字確定＆コミット処理.
-                    break;
-                default:
-                    enact = false;
-            }
-        }
-        if( enact ) InitialKana();      // convKanaの初期化.
-        if( keyData.code === "IntlRo" ) enact = true;    // IntlRo はハンドリング済に.
-    } else {
-        clearCompoAndCand();
-        if( imemode == 7 && keyData.key === "Esc" && keyData.altKey )
-            MakeTextNiwadictionary();             // 辞書のテキスト出力.
-    }
-    return enact;
 }
 
 // 一文字確定処理.
